b6ad20a683d8687e48ec9cb46ba97cc7c3817653
Brobbey <wirekobrobbey27@gmail.com>
2022-08-31 12:06:32 +0000

    calculateFactorial method implementation complete

--- src/com/spero/Factorial.java
+++ src/com/spero/Factorial.java
@@ -11,5 +11,16 @@ package src.com.spero;
 
         }
 
+        public int calulateFactorial(int length){
+            int total = 1;
+                    for (int i=1; i < length; i++){
+                        total *= i;
+                    }
+                    
+                    this.factorial = total;
+
+                    return this.factorial;
+    
+        }
         
     }
